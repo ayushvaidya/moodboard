fdfff157704fece5759e26e1ec51062d175c07bb
Ayush Vaidya <ayush.everest@gmail.com>
2018-12-29 13:39:53 -0500

    heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :category
   belongs_to :user
+
+  has_one_attached :image
+  validates :image, :presence => true
+
+  def correct_document_mime_type
+    if avatar.attached? && !avatar.content_type.in?(%w(image/jpg image/jpeg image/gif image/png))
+      errors.add(:image, 'Must be an image file (jpg, jpeg, gif or png)')
+    end
+  end
 end
