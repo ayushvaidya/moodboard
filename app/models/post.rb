fb92462d9a3282043c95008b70a0487e66e9b817
Ayush Vaidya <ayush.everest@gmail.com>
2018-12-30 21:14:54 -0500

    no image error

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,10 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
-  validates :image, :presence => true
   validates :category, :presence => true
 
   def correct_document_mime_type
-    if avatar.attached? && !avatar.content_type.in?(%w(image/jpg image/jpeg image/gif image/png))
+    if image.attached? && !image.content_type.in?(%w(image/jpg image/jpeg image/gif image/png))
       errors.add(:image, 'Must be an image file (jpg, jpeg, gif or png)')
     end
   end
